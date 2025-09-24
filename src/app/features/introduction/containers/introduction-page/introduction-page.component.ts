b32a7f9aa8c3c355b1d2ff598fe26211fdf27cfe
Adam Lany <adam.lany@posam.sk>
2025-09-24 18:48:07 +0200

    chore: fix after update

--- src/app/features/introduction/containers/introduction-page/introduction-page.component.ts
+++ src/app/features/introduction/containers/introduction-page/introduction-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-introduction-page',
   imports: [],
-  templateUrl: '. /introduction-page.component.html',
+  templateUrl: './introduction-page.component.html',
   styleUrl: './introduction-page.component.scss',
 })
 export class IntroductionPageComponent {}
